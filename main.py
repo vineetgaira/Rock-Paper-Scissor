3e6b710e8ecb84357bc6965dd0a4067d8aa01c11
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-01 11:39:22 +0530

    Added game logic

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
 import random
 
-def main():
-    pass
- 
-def get_user_choice():
-    pass
+def display_menu():
+    print("Here are your choices.")
+    print("1 : Rock")
+    print("2 : Paper")
+    print("3 : Scissor")
+    print("4 : Display score")
+    print("5 : Exit")
+
+def get_user_choice(user_choice):
+
+    user_choice=int(input("Enter your choice :")) 
+    if user_choice in range(1,6):
+        print("Valid.")
+    else:
+        print("Please enter a valid choice from the given menu.")
+    
+    return user_choice
 
 def get_computer_choice():
-    pass
+    
+    computer=random.randint(1,3)
+
+    return computer
+    
 
 def decide_winner():
-    pass
-
-def update_score():
-    pass
-
+    
+    if get_computer_choice()-get_user_choice()==0:
+        return "It's a draw."
+    elif (get_computer_choice()-get_user_choice()) % 3 != 1:
+        return  f"You Win.\nComputer chose {get_computer_choice()} You chose {get_user_choice()}."
+    elif (get_computer_choice()-get_user_choice()) % 3 == 1:
+        return  f"You Lose.\nComputer chose {get_computer_choice()} You chose {get_user_choice()}."
+    
 def display_score():
     pass
+    
+def main():
+    pass
+    
+
         
 
 
-
     
 
 
@@ -33,29 +56,3 @@ def display_score():
 
 
 
-# computer_choice={1:"Rock",
-#       2:"Paper",
-#       3:"Scissor"}
-
-
-# win=0
-# loss=0
-# while True:
-
-#     computer=random.randint(1,3)
-
-    
-#     user_choice=int(input("Enter your choice :"))
-
-#     if computer-user_choice == 0:
-#         print(f"This game is a draw.\nYou selected {computer_choice[user_choice]} computer selected {computer_choice[computer]}.")
-#     elif (computer - user_choice) % 3 !=1:
-#         print(f"You win.\nYou chose {computer_choice[user_choice]} computer chose {computer_choice[computer]}.")
-#         win+=1
-#     elif (computer - user_choice) % 3 ==1:
-#         print(f"You Lose.\nYou chose {computer_choice[user_choice]} computer chose {computer_choice[computer]}.")
-#         loss+=1
-#     elif user_choice==4:
-#         print(f"Wins:{win}\nLosses:{loss}")
-#     elif user_choice==5:
-#         break
