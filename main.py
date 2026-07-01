d215f99acb8016692ac812499536f495644db579
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-01 11:08:32 +0530

    Write clean code and debug

--- main.py
+++ main.py
@@ -1,51 +1,44 @@
-''' So it's pretty much simple.
-if Rock-Paper: Paper Wins
-Paper-Scissor: Scissor Wins
-Scissor-Rock-Rock Wins'''
-
-# Now what I can do is I can assign them a number
-# Rock : 1
-# Paper : 2
-# Scissor: 3
 import random
 
-cc={1:"Rock",
-      2:"Paper",
-      3:"Scissor"}
-
-revcc={"Rock":1,
-      "Paper":2,
-      "Scissor":3}
-
-computer=random.randint(1,3)
-
-x=revcc[cc[computer]]
-
-win=0
-loss=0
-while True:
-
+def game_menu():
     print("You have three choices.")
     print("1 : Rock")
     print("2 : Paper")
     print("3 : Scissor")
     print("4 : Display Wins/Loss")
     print("5 : Exit")
+
+def computer_side_logic():
+
+    computer_choices={1:"Rock",
+                      2:"Paper",
+                      3:"Scissor"}
     
-    user_choice=int(input("Enter your choice :"))
+    computer_select=random.randint(1,3)
 
-    if x-user_choice == 0:
-        print(f"This game is a draw.\nYou selected {cc[user_choice]} computer selected {cc[computer]}.")
-    elif x-user_choice== 1 or -2:
-        print(f"You win.\nYou chose {cc[user_choice]} computer chose {cc[computer]}.")
-        win+=1
-    elif user_choice-x== -1 or 2:
-        print(f"You Lose.\nYou chose {cc[user_choice]} computer chose {cc[computer]}.")
-        loss+=1
-    elif user_choice==4:
-        print(f"Wins:{win}\nLosses:{loss}")
-    elif user_choice==5:
-        break
+    return computer_select
+
+def user_choice_validation(user_choice):
+    if user_choice in range(1,6):
+        return "Valid"
+    else:
+        return "Invalid choice.\nPlease enter a integer from menu."
+
+def game_logic(user_choice,computer_select):
+
+    win=0
+    loss=0
+    draw=0
+    total=0
+    while True:
+
+        
+
+
+
+    
+
+
     
 
 
@@ -54,3 +47,30 @@ while True:
 
 
 
+
+# computer_choice={1:"Rock",
+#       2:"Paper",
+#       3:"Scissor"}
+
+
+# win=0
+# loss=0
+# while True:
+
+#     computer=random.randint(1,3)
+
+    
+#     user_choice=int(input("Enter your choice :"))
+
+#     if computer-user_choice == 0:
+#         print(f"This game is a draw.\nYou selected {computer_choice[user_choice]} computer selected {computer_choice[computer]}.")
+#     elif (computer - user_choice) % 3 !=1:
+#         print(f"You win.\nYou chose {computer_choice[user_choice]} computer chose {computer_choice[computer]}.")
+#         win+=1
+#     elif (computer - user_choice) % 3 ==1:
+#         print(f"You Lose.\nYou chose {computer_choice[user_choice]} computer chose {computer_choice[computer]}.")
+#         loss+=1
+#     elif user_choice==4:
+#         print(f"Wins:{win}\nLosses:{loss}")
+#     elif user_choice==5:
+#         break
