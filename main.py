00c6ee0701b2addf6db11f5346af5f3bb96cf300
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-01 12:07:21 +0530

    Debug and write clean code.

--- main.py
+++ main.py
@@ -8,38 +8,40 @@ def display_menu():
     print("4 : Display score")
     print("5 : Exit")
 
-def get_user_choice(user_choice):
+def get_user_choice():
 
     user_choice=int(input("Enter your choice :")) 
     if user_choice in range(1,6):
-        print("Valid.")
+        return user_choice
     else:
-        print("Please enter a valid choice from the given menu.")
-    
-    return user_choice
+        return "Please enter a valid choice. Choose a number from the given menu."
+
+user_choice = get_user_choice()
 
 def get_computer_choice():
     
     computer=random.randint(1,3)
 
     return computer
-    
 
-def decide_winner():
+computer_choice = get_computer_choice()
+
+def decide_winner(user_choice,computer_choice):
     
-    if get_computer_choice()-get_user_choice()==0:
+    if computer_choice - user_choice ==0:
         return "It's a draw."
-    elif (get_computer_choice()-get_user_choice()) % 3 != 1:
-        return  f"You Win.\nComputer chose {get_computer_choice()} You chose {get_user_choice()}."
-    elif (get_computer_choice()-get_user_choice()) % 3 == 1:
-        return  f"You Lose.\nComputer chose {get_computer_choice()} You chose {get_user_choice()}."
+    elif (computer_choice-user_choice) % 3 != 1:
+        return  f"You Win.\nComputer chose {computer_choice} You chose {user_choice}."
+    elif (computer_choice()-user_choice()) % 3 == 1:
+        return  f"You Lose.\nComputer chose {computer_choice} You chose {user_choice}."
     
 def display_score():
     pass
     
 def main():
     pass
-    
+
+main()
 
         
 
